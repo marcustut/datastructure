60b5f1f56e0fd3ad0bcb902af68c92f86a70d12c
Marcus Lee <marcustutorial@hotmail.com>
2024-03-24 15:29:04 +0000

    feat: used hex code for encoding

--- huffman/app/src/main/java/huffman/Huffman.java
+++ huffman/app/src/main/java/huffman/Huffman.java
@@ -4,13 +4,50 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 
+import huffman.Huffman;
+
 public class Huffman {
+    public static final int MAX_DEPTH = 32; // because we use Integer to represent the code (Integer is 32-bit)
+
+    private static class Code {
+        static final int DIGITS = 6;
+        int code;
+        int numBits;
+
+        Code(int code) {
+            this.code = code;
+        }
+
+        Code(int code, int numBits) {
+            this.code = code;
+            this.numBits = numBits;
+        }
+
+        public static Code fromString(String str) {
+            if (str.length() != DIGITS)
+                System.err.println("wrong digits for Code");
+
+            return new Code(
+                    Integer.parseInt(str.substring(0, 4), 16),
+                    Integer.parseInt(str.substring(DIGITS - 2, DIGITS), 16));
+        }
+
+        public String path() {
+            return Integer.toBinaryString((1 << numBits) | code).substring(1);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%1$04X", code) + String.format("%1$02X", numBits);
+        }
+    }
+
     public static class Encoded {
         public String encoded;
-        public HashMap<Character, String> codes;
+        public HashMap<Character, Code> codes;
         public Node root;
 
-        public Encoded(String encoded, HashMap<Character, String> codes, Node root) {
+        public Encoded(String encoded, HashMap<Character, Code> codes, Node root) {
             this.encoded = encoded;
             this.codes = codes;
             this.root = root;
@@ -22,7 +59,7 @@ public class Huffman {
         }
     }
 
-    public static class Node implements Comparable<Node> {
+    public static class Node implements Comparable<Node>, BinaryTreePrinter.PrintableNode {
         int freq;
         char c;
         Node left, right;
@@ -40,6 +77,22 @@ public class Huffman {
             this.freq = freq;
         }
 
+        public int depth() {
+            // Leaf node has a depth of 1
+            if (left == null && right == null)
+                return 1;
+
+            return depth(this);
+        }
+
+        private int depth(Node root) {
+            if (root == null) // reached the end
+                return 0;
+
+            // Take the maximum depth of left or right subtree
+            return Math.max(depth(root.left), depth(root.right)) + 1;
+        }
+
         @Override
         public int compareTo(Node o) {
             return freq - o.freq;
@@ -49,6 +102,22 @@ public class Huffman {
         public String toString() {
             return "{" + c + "=" + freq + "}";
         }
+
+        @Override
+        public BinaryTreePrinter.PrintableNode getLeft() {
+            return left;
+        }
+
+        @Override
+        public BinaryTreePrinter.PrintableNode getRight() {
+            return right;
+        }
+
+        @Override
+        public String getText() {
+            return toString();
+        }
+
     }
 
     public static Encoded encode(String payload) {
@@ -60,8 +129,14 @@ public class Huffman {
         // Build a huffman tree from the frequencies
         Node root = buildTree(freqMap);
 
+        // Print to STDERR if exceeds maximum depth
+        if (root.depth() > MAX_DEPTH)
+            System.err.printf(
+                    "Huffman Tree has a depth of %d which exceeds the maximum of %d, expect the encoded data to be wrong\n",
+                    root.depth(), MAX_DEPTH);
+
         // Traverse the tree to get the codes
-        HashMap<Character, String> codes = getCodesFromTree(root);
+        HashMap<Character, Code> codes = getCodesFromTree(root);
 
         // Encode the payload with codes
         String encoded = "";
@@ -72,21 +147,29 @@ public class Huffman {
     }
 
     public static String decode(Huffman.Node root, String encoded) {
+        if (encoded.length() % Code.DIGITS != 0)
+            System.err.printf("The size of the encoded string is not valid (must be a multiple of %d)\n", Code.DIGITS);
+
         String decoded = "";
         Node curr = root;
+        Code[] codes = new Code[encoded.length() / Code.DIGITS];
 
-        // Go through the encoded string and follow the path in the tree
-        for (int i = 0; i < encoded.length(); i++) {
-            if (encoded.charAt(i) == '0') // if it is '0' then go left
-                curr = curr.left;
-            else // else it is a '1', go right
-                curr = curr.right;
+        // Parse the decoded string into codes
+        for (int i = 0; i < encoded.length(); i += Code.DIGITS)
+            codes[i / Code.DIGITS] = Code.fromString(encoded.substring(i, i + Code.DIGITS));
 
-            // Once arriving at leaf node, we found the character so we reset
-            if (curr.left == null && curr.right == null) {
-                decoded += curr.c;
-                curr = root;
-            }
+        // Go through the codes and follow the path in the tree
+        for (Code code : codes) {
+            // Traverse the path
+            for (char c : code.path().toCharArray())
+                if (c == '0') // if it is '0' then go left
+                    curr = curr.left;
+                else // else it is a '1', go right
+                    curr = curr.right;
+
+            // Get character from the current path and reset
+            decoded += curr.c;
+            curr = root;
         }
 
         return decoded;
@@ -139,41 +222,42 @@ public class Huffman {
 
     private static Node buildTree(HashMap<Character, Integer> freqMap) {
         // Convert these frequencies as a Node and put them in a queue
-        MinHeap<Node> queue = new MinHeap<>();
-        freqMap.forEach((c, freq) -> queue.add(new Node(c, freq)));
+        MinHeap<Node> heap = new MinHeap<>();
+        freqMap.forEach((c, freq) -> heap.add(new Node(c, freq)));
 
-        // Consume the nodes in queue to construct a tree
-        while (queue.size() > 1) {
-            Node min = queue.poll();
-            Node nextMin = queue.poll();
+        // Consume the nodes in heap to construct a tree
+        while (heap.size() > 1) {
+            Node min = heap.poll();
+            Node nextMin = heap.poll();
 
             Node aggregated = new Node(min.freq + nextMin.freq);
             aggregated.left = min;
             aggregated.right = nextMin;
 
-            queue.add(aggregated);
+            heap.add(aggregated);
         }
 
-        return queue.poll();
+        // Return the root (the only node remaining in heap)
+        return heap.poll();
     }
 
-    private static HashMap<Character, String> getCodesFromTree(Node root) {
-        HashMap<Character, String> codes = new HashMap<>();
-        getCodesFromTree(root, codes, "");
+    private static HashMap<Character, Code> getCodesFromTree(Node root) {
+        HashMap<Character, Code> codes = new HashMap<>();
+        getCodesFromTree(root, codes, new Code(0));
         return codes;
     }
 
-    private static void getCodesFromTree(Node root, HashMap<Character, String> codes, String str) {
+    private static void getCodesFromTree(Node root, HashMap<Character, Code> codes, Code code) {
         // Base case: arrive at leaf node (is a character node)
         if (root.left == null && root.right == null) {
-            codes.put(root.c, str);
+            codes.put(root.c, code);
             return;
         }
 
         // Traverse left subtree
-        getCodesFromTree(root.left, codes, str + "0");
+        getCodesFromTree(root.left, codes, new Code((code.code << 1) | 0, code.numBits + 1));
 
         // Traverse right subtree
-        getCodesFromTree(root.right, codes, str + "1");
+        getCodesFromTree(root.right, codes, new Code((code.code << 1) | 1, code.numBits + 1));
     }
 }
--- huffman/app/src/main/java/huffman/example/App.java
+++ huffman/app/src/main/java/huffman/example/App.java
@@ -2,6 +2,9 @@ package huffman.example;
 
 import java.io.File;
 import java.io.IOException;
+
+import huffman.BinaryTreePrinter;
+import huffman.Huffman;
 import huffman.HuffmanFile;
 
 public class App {
@@ -16,5 +19,32 @@ public class App {
                         "/src/main/resources/compressed.hm"),
                 new File(System.getProperty("user.dir") +
                         "/src/main/resources/decompressed.txt"));
+
+        // Huffman.Encoded encoded = Huffman.encode(
+        // """
+        // /* Malloc implementation for multiple threads without lock contention.
+        // Copyright (C) 1996-2024 Free Software Foundation, Inc.
+        // Copyright The GNU Toolchain Authors.
+        // This file is part of the GNU C Library.
+
+        // The GNU C Library is free software; you can redistribute it and/or
+        // modify it under the terms of the GNU Lesser General Public License as
+        // published by the Free Software Foundation; either version 2.1 of the
+        // License, or (at your option) any later version.
+
+        // The GNU C Library is distributed in the hope that it will be useful,
+        // but WITHOUT ANY WARRANTY; without even the implied warranty of
+        // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+        // Lesser General Public License for more details.
+
+        // You should have received a copy of the GNU Lesser General Public
+        // License along with the GNU C Library; see the file COPYING.LIB. If
+        // not, see <https://www.gnu.org/licenses/>. */
+        // """);
+        // BinaryTreePrinter.print(encoded.root);
+        // System.out.println("Depth: " + encoded.root.depth());
+        // System.out.println(encoded);
+
+        // System.out.println(Huffman.decode(encoded.root, encoded.encoded));
     }
 }
